b1a43004230ceeffb806836af5c1b9d78ef8b1ee
Strix <mrstriga@gmail.com>
2018-04-03 15:03:10 +0300

    Add some console code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('Hello');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Timer');
+console.timeEnd('Timer');
