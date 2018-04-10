acb62f2c35d1f13f819d9264d9407f0212e08267
Strix <mrstriga@gmail.com>
2018-04-10 18:14:01 +0300

    013 Object Literals

--- app.js
+++ app.js
@@ -1,66 +1,38 @@
-// 012 Arrays Array Methods
+// 013 Object Literals
 
-// Create some arrays
-const numbers = [43,56,33,23,44,36,5];
-const numbers2 = new Array(22,45,33,76,54);
-
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-const mixed = [22, 'Hello', true, undefined, null, {a:1, b:2}, new Date()];
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 35,
+  email: 'test@test.com',
+  hobbies: ['music', 'sports'],
+  address: {
+    city: 'Miami',
+    state: 'FL'
+  },
+  getBirthYear: function () {
+    return (new Date()).getFullYear() - this.age;
+  }
+}
 
 let val;
 
-// Get array length
-val = numbers.length;
+val = person;
+// Specific value
+val = person.firstName;
+val = person['lastName'];
+val = person.hobbies[1];
+val = person.address['city'];
+val = person.getBirthYear();
 
-// Check if is array
-val = Array.isArray(numbers);
 
-// Get single value
-val = numbers[0];
-
-// Insert into array
-numbers[2] = 100;
-
-// Find index of value
-val = numbers.indexOf(36);
-
-/* // MUTATING ARRAYS
-// Add on to end
-numbers.push(250);
-// Add on to front
-numbers.unshift(120);
-// Take off from end
-numbers.pop();
-// Take off from front
-numbers.shift();
-// Splice values
-numbers.splice(1,3);
-//Reverse 
-numbers.reverse(); */
-
-// Concatenate arrays
-val = numbers.concat(numbers2);
-
-// Sorting arrays
-/* val = fruit.sort();
-val = numbers.sort(); */
-
-/* // Use the compare func
-
-val = numbers.sort(function (x, y) {
-  return x - y;
-});
-
-val = numbers.sort(function (x, y) {
-  return y - x;
-}); */
-
-// Find
-function over50(num) {
-  return num > 50;
-}
-
-val = numbers.find(over50);
-
-console.log(numbers);
 console.log(val);
+
+const people = [
+  {name: 'John', age: 30},
+  {name: 'Mike', age: 23}
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
