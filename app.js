6e8830073a3d16065f2b09176a72fc422114b94e
Strix <mrstriga@gmail.com>
2018-04-11 19:09:19 +0300

    015 If Statements Comparison Operators

--- app.js
+++ app.js
@@ -1,22 +1,95 @@
-// 013 Object Literals
+// 015 If Statements Comparison Operators
 
+/* if (condition) {
+  
+} else {
+  
+} */
 
-val = person;
-// Specific value
-val = person.firstName;
-val = person['lastName'];
-val = person.hobbies[1];
-val = person.address['city'];
-val = person.getBirthYear();
+const id = '100';
 
-
-console.log(val);
-
-const people = [
-  {name: 'John', age: 30},
-  {name: 'Mike', age: 23}
-];
-
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i].name);
+/* // EQUAL TO
+if (id == 100) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
 }
+
+// NOT EQUAL TO
+if (id != 101) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
+}
+
+// EQUAL TO VALUE & TYPE
+if (id === 100) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
+}
+
+// NOT EQUAL TO VALUE & TYPE
+if (id !== 100) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
+} */
+
+// Test if undefined
+// if (typeof id !== 'undefined') {
+//   console.log(`The id is ${id}`);
+// } else {
+//   console.log('NO ID');
+// }
+
+// GREATER OR LESS THAN
+// if (id <= 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// IF ELSE
+
+const color = 'yellow';
+
+/*  if (color === 'red') {
+   console.log('Color is red');
+ } else if (color === 'blue') {
+   console.log('Color is blue');
+ }
+  else {
+    console.log('Color not red and blue');
+  } */
+
+  // LOGICAL OPERATORS
+
+  const name = 'Steve';
+  const age = 70;
+
+  // AND &&
+  if (age > 0 && age <= 12) {
+    console.log(`${name} is a child`);
+  } else if (age >=13 && age <= 19) {
+    console.log(`${name} is a teenager`);
+  } else {
+    console.log(`${name} is an adult`);
+  }
+
+  // OR ||
+  if (age < 16 || age > 65) {
+    console.log(`${name} can not run in race`);
+  } else {
+    console.log(`${name} is registred for the race`);
+  }
+
+// TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// WITHOUT BRACERS
+
+if (id === 100)
+  console.log('CORRECT');
+else
+  console.log('INCORRECT');
