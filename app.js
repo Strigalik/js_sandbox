8a89fcf337cbef4d90938ce54dd4832504d0de5c
Strix <mrstriga@gmail.com>
2018-04-12 15:33:43 +0300

    018 General Loops

--- app.js
+++ app.js
@@ -1,95 +1,71 @@
-// 015 If Statements Comparison Operators
-
-/* if (condition) {
-  
-} else {
-  
-} */
-
-const id = '100';
-
-/* // EQUAL TO
-if (id == 100) {
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-}
-
-// NOT EQUAL TO
-if (id != 101) {
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-}
-
-// EQUAL TO VALUE & TYPE
-if (id === 100) {
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-}
-
-// NOT EQUAL TO VALUE & TYPE
-if (id !== 100) {
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-} */
-
-// Test if undefined
-// if (typeof id !== 'undefined') {
-//   console.log(`The id is ${id}`);
-// } else {
-//   console.log('NO ID');
-// }
-
-// GREATER OR LESS THAN
-// if (id <= 100) {
-//   console.log('CORRECT');
-// } else {
-//   console.log('INCORRECT');
-// }
-
-// IF ELSE
-
-const color = 'yellow';
-
-/*  if (color === 'red') {
-   console.log('Color is red');
- } else if (color === 'blue') {
-   console.log('Color is blue');
- }
-  else {
-    console.log('Color not red and blue');
-  } */
-
-  // LOGICAL OPERATORS
-
-  const name = 'Steve';
-  const age = 70;
-
-  // AND &&
-  if (age > 0 && age <= 12) {
-    console.log(`${name} is a child`);
-  } else if (age >=13 && age <= 19) {
-    console.log(`${name} is a teenager`);
-  } else {
-    console.log(`${name} is an adult`);
+// 018 General Loops
+// FOR LOOP
+/* for (let i = 0; i < 10; i++) {
+  // console.log('Number ' + i);  
+  if (i === 2) {
+    console.log('2 is my favorite number');
+    continue;
   }
 
-  // OR ||
-  if (age < 16 || age > 65) {
-    console.log(`${name} can not run in race`);
-  } else {
-    console.log(`${name} is registred for the race`);
+  if (i === 5) {
+    console.log('Stop the loop');
+    break;
   }
+  console.log('Number' + i);
+} */
 
-// TERNARY OPERATOR
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+// WHILE LOOP
 
-// WITHOUT BRACERS
+// let i = 0;
 
-if (id === 100)
-  console.log('CORRECT');
-else
-  console.log('INCORRECT');
+// while (i < 100) {
+//   console.log('Number ' + i);
+//   i = i + 6;
+// }
+
+// DO WHILE
+
+// let i = 100;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// }
+// while(i < 10);
+
+// LOOP THROUGH ARRAY
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);;
+// }
+
+// FOREACH
+// cars.forEach(function(car, index, array){
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users = [
+//   {id:1, name: 'John'},
+//   {id:2, name: 'Sara'},
+//   {id:3, name: 'Karen'},
+// ];
+
+// const ids = users.map(function(user) {
+//   return user.id;
+// });
+
+// console.log(ids);
+
+// FOR IN LOOP
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 30
+}
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
