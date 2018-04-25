bf4b99982a0a398582c5e780fde118e3845eb033
Strix <mrstriga@gmail.com>
2018-04-25 19:16:22 +0300

    // 028 Event Listeners The Event Object

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
-// 022 Examining The Document Object
-let val;
+// 028 Event Listeners The Event Object
 
-val = window;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.URL;
-
-val = document.forms;
-val = document.forms[0].id;
-val = document.forms[0].action;
-
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
-
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(script => {
-  console.log(script.getAttribute('src'));
+document.querySelector('.clear-tasks').addEventListener('click', function(e) {
+	console.log('Hello');
+	e.preventDefault();
 });
-
-console.log(val);
