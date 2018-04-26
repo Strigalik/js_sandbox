0f9e6e118708036fbb459cfacc8c7986a436abe2
Strix <mrstriga@gmail.com>
2018-04-26 15:49:21 +0300

    // 030 Keyboard  Input Events

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // 030 Keyboard  Input Events
 
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+
+// Clear input
+taskInput.value = '';
+
+// form.addEventListener('submit', runEvent);
+
+taskInput.addEventListener('keydown', runEvent);
+
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+  console.log(e.target.value);
+  console.log(e.target.value.length);
+  // console.log(taskInput.value);
+
+  // e.preventDefault();
+}
