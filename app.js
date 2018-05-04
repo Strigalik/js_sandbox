6040e62a6f800667087045f9f585da1c8f7a16f1
Strix <mrstriga@gmail.com>
2018-05-04 16:46:33 +0300

    032 Local Session Storage

--- app.js
+++ app.js
@@ -19,7 +19,20 @@
 // console.log(name);
 
 document.querySelector('form').addEventListener('submit', function(e){
-  const task = document.getElementById('task').value;
-  console.log(task);
+	const task = document.getElementById('task').value;
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.push(task);
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+	alert('Task saved');
   e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+tasks.forEach(task => {
+	console.log(task);
 });
