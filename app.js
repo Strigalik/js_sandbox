2bf7d26e5d57a04a7ffbbca16ab358b6927f68fb
Strix <mrstriga@gmail.com>
2018-05-07 19:12:49 +0300

    034 Task List Part 2 - Delete  Filter Tasks

--- app.js
+++ app.js
@@ -10,36 +10,77 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
-  // Add task event
-  form.addEventListener('submit', addTask);
+	// Add task event
+	form.addEventListener('submit', addTask);
+	// Remove task event
+	taskList.addEventListener('click', removeTask);
+	// Clear task event
+	clearBtn.addEventListener('click', clearTasks);
+	// Filter Tasks event
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
 function addTask(e) {
-  if(taskInput.value === '') {
-    alert('Add a task');
-  }
+	if(taskInput.value === '') {
+		alert('Add a task');
+		return false;
+	}
+	
+	// Create li element
+	const li = document.createElement('li');
+	// Add class
+	li.className = 'collection-item';
+	// Create text node and append to li
+	li.appendChild(document.createTextNode(taskInput.value));
+	// Create new link element
+	const link = document.createElement('a');
+	// Add class
+	link.className = 'delete-item secondary-content';
+	// Add icon html
+	link.innerHTML = '<i class="fa fa-remove"></i>';
+	// Append the link to li
+	li.appendChild(link);
+	
+	// Append li to ul
+	taskList.appendChild(li);
+	
+	// Clear input
+	taskInput.value = '';
+	
+	e.preventDefault();
+}
 
-  // Create li element
-  const li = document.createElement('li');
-  // Add class
-  li.className = 'collection-item';
-  // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
-  const link = document.createElement('a');
-  // Add class
-  link.className = 'delete-item secondary-content';
-  // Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append the link to li
-  li.appendChild(link);
+// Remove Task
+function removeTask(e) {
+	if (e.target.parentElement.classList.contains('delete-item')) {
+		if (confirm('Are You Sure')) {
+			e.target.parentElement.parentElement.remove();
+		}
+	}
+}
 
-  // Append li to ul
-  taskList.appendChild(li);
+// Clear Tasks
+function clearTasks() {
+	// taskList.innerHTML = '';
 
-  // Clear input
-  taskInput.value = '';
+	// Faster
+	if (confirm('All Tasks Will Be Deleted!')) {
+		while (taskList.firstChild) {
+			taskList.removeChild(taskList.firstChild);
+		}
+	}
+}
 
-  e.preventDefault();
+// Filter Tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+	document.querySelectorAll('.collection-item').forEach(function (task) {
+		const item = task.firstChild.textContent;
+		if (item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
 }
