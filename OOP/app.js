81269f980181baaccd20c60a049e5a616216b43c
Strix <mrstriga@gmail.com>
2018-05-15 18:27:19 +0300

    047 Using Object.create

--- OOP/app.js
+++ OOP/app.js
@@ -113,7 +113,7 @@ console.log(mary.hasOwnProperty('firstName')); */
  */
 
 // Person constructor
-function Person(firstName, lastName) {
+/* function Person(firstName, lastName) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 }
@@ -150,4 +150,35 @@ Customer.prototype.greeting = function () {
 	return `Hello there ${this.firstName} ${this.lastName} welcome to our company`
 }
 
-console.log('customer1.greeting :', customer1.greeting());
+console.log('customer1.greeting :', customer1.greeting()); */
+
+/**
+ *  047 Using Object.create
+ */
+
+const personPrototypes = {
+	greeting: function () {
+		return `Hello there ${this.firstName} ${this.lastName}`
+	},
+	getsMarried: function (newLastName) {
+		this.lastName = newLastName;
+	}
+}
+
+const mary = Object.create(personPrototypes);
+mary.firstName = 'Mary';
+mary.lastName = 'Williams';
+mary.age = 30;
+
+mary.getsMarried('Thompson');
+
+console.log('mary :', mary);
+console.log('mary.greeting() :', mary.greeting());
+
+const brad = Object.create(personPrototypes, {
+	firstName: { value: 'Brad' },
+	lastName: { value: 'Traversy' },
+	age: { value: 36 }
+});
+
+console.log('brad :', brad);
