4237a770642beb554ccf84cd04670f0871fe3e3a
Strix <mrstriga@gmail.com>
2018-05-30 17:14:05 +0300

    Finished 065 ES6 Promises

--- Asynchronous JavaScript Ajax Fetch API/065-app.js
+++ Asynchronous JavaScript Ajax Fetch API/065-app.js
@@ -1,3 +1,4 @@
+;
 const posts = [
 	{ title: 'Post One', body: 'This is post one' },
 	{ title: 'Post Two', body: 'This is post two' },
@@ -23,12 +24,18 @@ createPost({ title: 'Post Three', body: 'This is post three' });
 
 getPosts(); */
 
-function createPost(post, callback) {
-	setTimeout(() => {
-		posts.push(post);
-		callback();
-		console.log('createpost');
-	}, 100);
+function createPost(post) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(() => {
+			posts.push(post);
+			const error = true;
+			if (!error) {
+				resolve();
+			} else {
+				reject('Error: Something went wrong');
+			}
+		}, 100);
+	});
 }
 
 function getPosts() {
@@ -38,8 +45,11 @@ function getPosts() {
 			output += `<li>${post.title}</li>`
 		})
 		document.body.innerHTML = output;
-		console.log('getPosts');
 	}, 500);
 }
 
-createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
+createPost({ title: 'Post Three', body: 'This is post three' })
+.then(getPosts)
+.catch(function (err) {
+	console.log(err);
+});
