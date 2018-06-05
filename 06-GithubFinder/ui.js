dcad63b318f8b8e0aeaa7e18ef96a0676c7d7581
Strix <mrstriga@gmail.com>
2018-06-05 18:16:14 +0300

    Fixed whitespaces

--- 06-GithubFinder/ui.js
+++ 06-GithubFinder/ui.js
@@ -9,7 +9,6 @@ class UI {
 					<div class="col-md-3">
 						<img class="img-fluid mb-2" src="${user.avatar_url}">
 						<a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
-
 					</div>
 					<div class="col-md-9">
 						<span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
