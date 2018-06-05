b27c96200bd9908cd374fb16a9eca73f25b8887d
Strix <mrstriga@gmail.com>
2018-06-05 17:59:34 +0300

    Github Finder Part 3 - Display The Profile TimeStamp 05:06

--- 06-GithubFinder/app.js
+++ 06-GithubFinder/app.js
@@ -1,5 +1,7 @@
 // Init Github
 const github = new GitHub;
+// Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -18,11 +20,11 @@ searchUser.addEventListener('keyup', (e) => {
 
 				} else {
 					// Show profile
-
+					ui.showProfile(data.profile);
 				}
 			})
 	} else {
 		// Clear profile
-		
+
 	}
 });
