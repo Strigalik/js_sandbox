bae5decfdcc7e40861668c13c23da74bc1288d54
Strix <mrstriga@gmail.com>
2018-06-07 14:51:34 +0300

    Finished project

--- 06-GithubFinder/github.js
+++ 06-GithubFinder/github.js
@@ -2,13 +2,18 @@ class GitHub {
 	constructor() {
 		this.client_id = '7ec686a50a51db87eca0';
 		this.client_secret = '11885cebfb5f69382bbf97382c7da09411b10e63';
+		this.repos_count = 5;
+		this.repos_sort = 'created: asc';
 	}
 
 	async getUser(user) {
 		const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+		const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 		const profile = await profileResponse.json();
+		const repos = await repoResponse.json();
 		return {
-			profile
+			profile,
+			repos
 		}
 	}
 }
--- 06-GithubFinder/ui.js
+++ 06-GithubFinder/ui.js
@@ -12,10 +12,10 @@ class UI {
 						<a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
 					</div>
 					<div class="col-md-9">
-						<span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-						<span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-						<span class="badge badge-success">Followers: ${user.followers}</span>
-						<span class="badge badge-info">Following: ${user.following}</span>
+						<span class="badge badge-primary mb-2">Public Repos: ${user.public_repos}</span>
+						<span class="badge badge-secondary mb-2">Public Gists: ${user.public_gists}</span>
+						<span class="badge badge-success mb-2">Followers: ${user.followers}</span>
+						<span class="badge badge-info mb-2">Following: ${user.following}</span>
 						<br><br>
 						<ul class="list-group">
 							<li class="list-group-item"><strong>Company:</strong> ${user.company}</li>
@@ -30,6 +30,33 @@ class UI {
 			<div id="repos"></div>
 		`;
 	}
+
+	// Show user repos
+	showRepos(repos) {
+		let output = '';
+
+		repos.forEach((repo) => {
+			output += `
+			<div class="card card-body mb-2">
+				<div class="row">
+					<div class="col-md-6 mb-2">
+						<a href="${repo.html_url}" target="_blank">${repo.name}</a>
+					</div>
+					<div class="col-md-6">
+						<span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+						<span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+						<span class="badge badge-success">Forks: ${repo.forks_count}</span>
+					</div>
+				</div>
+			</div>
+			`;
+		});
+
+		// Output repos
+		document.getElementById('repos').innerHTML = output;
+
+	}
+
 	// Show alert message
 	showAlert(message, className) {
 		// Clear any remaining alerts
